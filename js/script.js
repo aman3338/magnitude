520a6302280e8f1f1d732515351f18a7f66482ec
aman3338 <amanpreet3338@gmail.com>
2020-09-08 13:12:19 +0530

    New changes happend.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const navSlide = () => {
+    const lines = document.querySelector('.lines');
+    const nav = document.querySelector('.links');
+    const link = document.querySelectorAll('.link');
+
+    lines.addEventListener('click', () => {
+        
+        nav.classList.toggle('active');
+        link.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 + 2}s`;
+            }
+        });
+        lines.classList.toggle('toggle');
+    });
+
+};
+
+navSlide();
